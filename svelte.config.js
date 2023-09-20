a24bff717da1540700461758edbbe86bb293afb4
Ben Wathen <bmwathen96@gmail.com>
2023-09-20 08:53:48 -0500

    Add cloudflare to project

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,9 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-cloudflare-workers';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({}),
 		csrf: {
 			checkOrigin: false, // Throws "Cross-site POST form submissions are forbidden" without
 		}
